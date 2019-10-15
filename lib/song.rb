473b5ed1f03deda6a854d7fab38267ec55680fdb
Jonathan Norman <jnorman2123@gmail.com>
2019-10-15 13:38:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,7 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
+  @@genre = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -9,5 +10,6 @@ class Song
     @genre = genre
     @@count += 1
     @@artists << artist
+    @@genre << genre
   end
 end
